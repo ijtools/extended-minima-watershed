4926b5c2be26d316d0a7eccd080fca1b987ad2a2
dlegland <david.legland@inrae.fr>
2026-04-14 14:37:48 +0200

    add minimalist implementation

---
+++ src/main/java/net/ijt/extminwat/ExtendedMinimaWatershedPlugin.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package net.ijt.extminwat;
+
+import ij.IJ;
+import ij.plugin.PlugIn;
+
+/**
+ * 
+ */
+public class ExtendedMinimaWatershedPlugin implements PlugIn
+{
+
+    @Override
+    public void run(String arg)
+    {
+        IJ.showMessage("Extended Minima Watershed", "I will soon run extended minima watershed!");
+    }
+
+}
